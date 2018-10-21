0afb88bbca5fb34fbd082169f784eba300ee2fe9
softwaresaiyajin <geralda.adorza@gmail.com>
2018-10-21 13:15:28 +0800

    Added router and data modules

---
+++ Modules/DataModule/DataModule/DataModule.h
@@ -0,0 +1,19 @@
+//
+//  DataModule.h
+//  DataModule
+//
+//  Created by Gerald Adorza on 21/10/2018.
+//  Copyright © 2018 Gerald Adorza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataModule.
+FOUNDATION_EXPORT double DataModuleVersionNumber;
+
+//! Project version string for DataModule.
+FOUNDATION_EXPORT const unsigned char DataModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataModule/PublicHeader.h>
+
+
